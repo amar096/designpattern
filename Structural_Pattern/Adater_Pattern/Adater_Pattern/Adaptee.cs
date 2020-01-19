24aa7bcba38ada50e40592ad549a5ab0dad881f1
amar <amar.roy.ext@sivantos.com>
2020-01-19 07:46:32 +0530

    Adapter Pattern

---
+++ Structural_Pattern/Adater_Pattern/Adater_Pattern/Adaptee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adater_Pattern
+{
+    class Adaptee : IAdaptee
+    {
+        public void EuropePlugin()
+        {
+            Console.WriteLine("European Plugin System");
+        }
+    }
+}
