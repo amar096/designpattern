5db506621b0fdc4fe230d1f582003ee462ce6edb
amar <amar.roy.ext@sivantos.com>
2020-01-19 08:56:18 +0530

    Facade Pattern

---
+++ Structural_Pattern/Facade_Pattern/Facade_Pattern/NonVegRestaurants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade_Pattern
+{
+    public class NonVegRestaurants
+    {
+        public Menu GetMenu()
+        {
+            NonVeg nonVeg = new NonVeg();
+            return nonVeg;
+        }
+    }
+}
